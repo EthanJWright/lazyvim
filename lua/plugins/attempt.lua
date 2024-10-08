43c4ee6881a3436778b73f9392db5dc559972d48
EthanJWright <ewright362@gmail.com>
2024-10-08 09:31:48 -0600

    add ts to attempt

--- lua/plugins/attempt.lua
+++ lua/plugins/attempt.lua
@@ -6,6 +6,15 @@ return {
     "nvim-telescope/telescope-ui-select.nvim",
   },
   config = function()
-    require("attempt").setup()
+    require("attempt").setup({
+      dir = vim.fn.expand("~") .. "/.attempt/",
+      ext_options = { "lua", "js", "py", "ts" },
+      run = {
+        lua = { "w !lua" },
+        py = { "w !python3" },
+        js = { "w !node" },
+        ts = { "w !ts-node" },
+      },
+    })
   end,
 }
