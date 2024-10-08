a56e4197207d67339b601f026371201afe5aa191
EthanJWright <ewright362@gmail.com>
2024-10-08 09:31:54 -0600

    add egrepify

--- lua/plugins/telescope-egrepify.lua
+++ lua/plugins/telescope-egrepify.lua
@@ -1,3 +1,4 @@
+-- https://github.com/fdschmidt93/telescope-egrepify.nvim
 return {
   "fdschmidt93/telescope-egrepify.nvim",
   dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
