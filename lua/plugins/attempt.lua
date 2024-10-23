5f21310ea404f64b8d3d625e0e065a551f714c68
EthanJWright <ewright362@gmail.com>
2024-10-23 13:28:20 -0600

    use correct attempt dir

--- lua/plugins/attempt.lua
+++ lua/plugins/attempt.lua
@@ -6,9 +6,10 @@ return {
     "nvim-telescope/telescope-ui-select.nvim",
   },
   config = function()
+    local attempts = require("custom.attempts")
     require("attempt").setup({
       -- make ~/.attempt and set up typescript in it (npm init, npm i typescript, npx tsc --init)
-      dir = vim.fn.expand("~") .. "/.attempt/",
+      dir = attempts.attempt_dir,
       ext_options = { "lua", "js", "py", "ts" },
       run = {
         lua = { "w !lua" },
