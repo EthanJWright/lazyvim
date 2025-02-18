c92af5cd6ffcfe3f27255464855835f3776010fd
EthanJWright <ewright362@gmail.com>
2025-02-18 15:22:27 -0700

    move neotest config back, move dap td to dap.lua

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,6 @@
+return {
+  "mfussenegger/nvim-dap",
+  keys = {
+    { "<leader>td", vim.NIL },
+  },
+}
--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,6 +1,45 @@
 return {
-  "mfussenegger/nvim-dap",
-  keys = {
-    { "<leader>td", vim.NIL },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "haydenmeade/neotest-jest",
+      "marilari88/neotest-vitest",
+      "nvim-neotest/nvim-nio",
+    },
+    keys = {
+      {
+        "<leader>tl",
+        function()
+          require("neotest").run.run_last()
+        end,
+        desc = "Run Last Test",
+      },
+      {
+        "<leader>tL",
+        function()
+          require("neotest").run.run_last({ strategy = "dap" })
+        end,
+        desc = "Debug Last Test",
+      },
+      {
+        "<leader>tw",
+        "<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
+        desc = "Run Watch",
+      },
+    },
+    opts = function(_, opts)
+      table.insert(
+        opts.adapters,
+        require("neotest-jest")({
+          jestCommand = "npm test --",
+          jestConfigFile = "custom.jest.config.ts",
+          env = { CI = true },
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        })
+      )
+      table.insert(opts.adapters, require("neotest-vitest"))
+    end,
   },
 }
